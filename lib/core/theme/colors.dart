c2fd138c21e794f6e31b64b3e9bd3da3dcf67cb1
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2024-11-08 07:52:06 +0700

    feat(theme): create colors

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color primary = Color(0xFF438fca);
+  static const Color primaryDark = Color(0xFF2c61ad);
+  static const Color content = Color(0xFF5f7ccc);
+}
